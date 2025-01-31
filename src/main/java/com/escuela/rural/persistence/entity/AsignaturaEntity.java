c3a1b4776a5f263ce2dd037b783ae7d1ed644a62
Ronald-ICastroG_Acer <ronaldivan.1210868@correo.uis.edu.co>
2025-01-31 01:44:40 -0500

    AsingaturaEntityCreated

---
+++ src/main/java/com/escuela/rural/persistence/entity/AsignaturaEntity.java
@@ -0,0 +1,28 @@
+package com.escuela.rural.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name="asignaturas")
+public class AsignaturaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="asg_id",columnDefinition = "BIGINT")
+    private Long asgId;
+
+    @Column(name="asg_name",nullable = false)
+    private String asgName;
+
+    @Column(name="asg_active",columnDefinition = "TINYINT",nullable = false)
+    private Boolean asgActive;
+
+    @ManyToOne(targetEntity = CursoEntity.class,fetch = FetchType.LAZY)
+    private CursoEntity curso;
+ }
--- src/main/java/com/escuela/rural/persistence/entity/CursoEntity.java
+++ src/main/java/com/escuela/rural/persistence/entity/CursoEntity.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 
 @Entity
 @Data
@@ -26,5 +28,8 @@ public class CursoEntity {
 
     @Column(name="cur_active",columnDefinition = "TINYINT",nullable = false)
     private Boolean curActive;
+
+    @OneToMany(targetEntity = AsignaturaEntity.class,mappedBy = "curso",fetch = FetchType.LAZY)
+    private List<AsignaturaEntity> asignaturas;
 
 }
