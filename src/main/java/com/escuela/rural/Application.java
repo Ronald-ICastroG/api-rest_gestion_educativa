beda84669750c24092ce1ca79ee41d068c44b6e4
Ronald-ICastroG_Acer <ronaldivan.1210868@correo.uis.edu.co>
2025-02-01 12:21:44 -0500

    master recuperado en la línea

--- src/main/java/com/escuela/rural/Application.java
+++ src/main/java/com/escuela/rural/Application.java
@@ -2,10 +2,15 @@ package com.escuela.rural;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+<<<<<<< HEAD
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+=======
+
+@SpringBootApplication
+>>>>>>> 99d8a11 (primer commit configuracion aplication.properties listo)
 public class Application {
 
 	public static void main(String[] args) {
