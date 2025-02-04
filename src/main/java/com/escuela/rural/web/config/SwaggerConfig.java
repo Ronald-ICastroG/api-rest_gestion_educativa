4441fa16d5400bd1beac92210a8851afb24f3a1f
Ronald-ICastroG_Acer <ronaldivan.1210868@correo.uis.edu.co>
2025-02-04 12:40:52 -0500

    Config/swaggerConfig created  in process

---
+++ src/main/java/com/escuela/rural/web/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.escuela.rural.web.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.servers.Servers;
+
+@OpenAPIDefinition(
+        info=@Info(
+                title="API Gestión educativa Escuela Rural",
+                description = "La presente api consiste en establecer los parámetros básicos de una institución educativa de carácter rural en colombia ",
+                termsOfService = "https://github.com/Ronald-ICastroG/api-rest_gestion_educativa",
+                version = "1.0.0",
+                summary = "La aplicación permite gestionar los aspectos básicos para la gestión educativa, creación de grupos, asignaturas, periodos, docentes, estuadiantes y calificaciones",
+                contact = @Contact(
+                         name="Ronald I. Castro G.",
+                        url = "https://github.com/Ronald-ICastroG",
+                        email = "ronaldivan.121068@correo.uis.edu.co"
+                ),
+                license = @License(
+                        name= "Standar Software Use License for Rc8.corp",
+                        url="https://github.com/Ronald-ICastroG/api-rest_gestion_educativa",
+                        identifier = "127.0.0.1"
+
+                )
+
+
+        ),servers ={
+                @Server(
+                        description = "DEV SERVER",
+                        url = "https://localhost:8080/dev/api/v1/geducativa"
+                ),
+                @Server(description = "PROD SERVER",
+                        url = "https://localhost:8080/prod/api/v1/geducativa"
+                )
+}
+)
+public class SwaggerConfig {
+}
